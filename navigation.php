b52e2d814f85a9dae3a75a4f7a24f0010f0dc519
HA-Sajib <102346661+HA-Sajib@users.noreply.github.com>
2022-06-12 08:01:31 -0800

    Add files via upload

---
+++ navigation.php
@@ -0,0 +1,74 @@
+
+
+<!-- Main navigation -->
+
+<div class="sidebar-category sidebar-category-visible">
+						<div class="category-content no-padding">
+							<ul class="navigation navigation-main navigation-accordion">
+
+								<!-- Main -->
+								<li class="navigation-header"><span>Main</span> <i class="icon-menu" title="Main pages"></i></li>
+								<li class="active"><a href="index.html"><i class="icon-home4"></i> <span>Dashboard</span></a></li>
+								<li class="#"><a href="banner.php"><i class="icon-image-compare"></i> <span>Banners</span></a></li>
+								<li class="#"><a href="index.html"><i class="icon-spinner2"></i> <span>Services</span></a></li>
+								<li class="#"><a href="index.html"><i class=" icon-flattr"></i> <span>Sections</span></a></li>
+								<li class="#"><a href="index.html"><i class="icon-googleplus5"></i> <span>Our Projects</span></a></li>
+								<li class="#"><a href="index.html"><i class="icon-man-woman"></i> <span>Our Staff</span></a></li>
+								<li class="#"><a href="index.html"><i class=" icon-height"></i> <span>Our Clients</span></a></li>
+								<li class="#"><a href="index.html"><i class="icon-phone-outgoing"></i> <span>Contact Message</span></a></li>
+								<li>
+									<a href="#"><i class="icon-gear"></i> <span>Back Office Setup</span></a>
+									<ul>
+										<li><a href="layout_navbar_fixed.html">Category</a></li>
+										<li><a href="layout_navbar_fixed.html">Designation</a></li>
+									</ul>
+								</li>
+								<li>
+									<a href="#"><i class="icon-copy"></i> <span>Layouts</span></a>
+									<ul>
+										<li><a href="index.html" id="layout1">Layout 1 <span class="label bg-warning-400">Current</span></a></li>
+										<li><a href="../../layout_2/LTR/index.html" id="layout2">Layout 2</a></li>
+									</ul>
+								</li>
+
+								<li>
+									<a href="#"><i class="icon-stack"></i> <span>Starter kit</span></a>
+									<ul>
+										<li><a href="starters/horizontal_nav.html">Horizontal navigation</a></li>
+										<li><a href="starters/1_col.html">1 column</a></li>
+									</ul>
+								</li>
+
+								<!-- /main -->
+
+								<!-- Forms -->
+
+								<!-- /forms -->
+
+								<!-- Appearance -->
+
+								<!-- /appearance -->
+
+								<!-- Layout -->
+
+								<!-- /layout -->
+
+								<!-- Data visualization -->
+
+								<!-- /data visualization -->
+
+								<!-- Extensions -->
+
+								<!-- /extensions -->
+
+								<!-- Tables -->
+
+								<!-- /tables -->
+
+								<!-- Page kits -->
+
+								<!-- /page kits -->
+
+							</ul>
+						</div>
+					</div>
